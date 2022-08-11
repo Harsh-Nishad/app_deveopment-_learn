9b1119a73d3197cd899bc37588a399b52a74337b
HARSH NISHAD <harshnishad80@gmail.com>
2022-08-11 23:04:42 +0530

    day13

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -4,11 +4,28 @@ import 'package:flutter/material.dart';
 import 'package:first_app/utils/Routes.dart';
 import 'package:first_app/widgets/drawer.dart';
 import 'package:first_app/widgets/item_widget.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class homepage extends StatelessWidget {
-  //final dummylist = List.generate(20, (index) => catalogmodel.items[0]);
-
+class homepage extends StatefulWidget {
   @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  //final dummylist = List.generate(20, (index) => catalogmodel.items[0]);
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loaddata();
+  }
+
+  loaddata() async {
+    final catalogJson = await rootBundle.loadString('assets/data/catalog.json');
+    final decodeddata = json.decode(catalogJson);
+    var productsdata = decodeddata['products'];
+  }
 
   //using the scaffold to create a page to feel the user interface
   //
